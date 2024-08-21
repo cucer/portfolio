e35b242f0c074aa01ce9f857cbb41581312d63e5
Cagatay Ucer <cagatay.ucer@gmail.com>
2024-08-22 01:11:28 +0300

    astro

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -85,7 +85,8 @@ const HeroContent = () => {
         className="w-full h-full flex justify-center items-center"
       >
         <Image
-          src="./mainIconsdark.svg"
+          // src="./mainIconsdark.svg"
+          src="./astro.png"
           alt="work icons"
           height={650}
           width={650}
