1fb14a80d17450da24af2d6048a95f0bae75c5bd
Cagatay Ucer <cagatay.ucer@gmail.com>
2024-08-22 14:07:30 +0300

    revize

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a href="#about" className="h-auto w-auto flex flex-row items-center">
-          {/* <Image
+          <Image
             style={{
               borderRadius: '50%',
             }}
@@ -18,7 +18,7 @@ const Navbar = () => {
             width={50}
             height={50}
             className="cursor-pointer hover:animate-slowspin"
-          /> */}
+          />
 
           <span
             className="font-bold ml-[10px] hidden md:block text-gray-300 text-transparent bg-clip-text
